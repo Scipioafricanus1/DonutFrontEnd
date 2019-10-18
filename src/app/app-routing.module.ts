1bd6f8beb2d8ecbd952b16d018b0bfda2b66cd71
Brent Harris <bdharris3@uh.edu>
2019-10-17 22:38:40 -0500

    added router. Need to add the old functionality from other DonutApp designs.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,11 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { CheckOutComponent } from './components/check-out/check-out.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'checkOut', component: CheckOutComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,5 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppComponent } from './app.component';
 import { ProductsComponent } from './components/products/products.component';
 import { CheckOutComponent } from './components/check-out/check-out.component';
